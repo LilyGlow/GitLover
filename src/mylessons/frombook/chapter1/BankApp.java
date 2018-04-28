70be55ad4adfe595a78f31f7183250facf5e5f6c
Oxygen_Intoxication <yu-b@yandex.ru>
2018-04-28 20:42:57 +0300

    Chapter 1 im still playing

--- src/mylessons/frombook/chapter1/BankApp.java
+++ src/mylessons/frombook/chapter1/BankApp.java
@@ -1,15 +1,15 @@
 package mylessons.frombook.chapter1;
-import java.util.Scanner;
+import java.util.*;
 
 
 
 public class BankApp
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
 
 
         System.out.print("Insert your balance please: ");
+        //Timer waiting = new Timer;
 
         double openingbalancein;
         Scanner doublein = new Scanner(System.in);
@@ -24,8 +24,18 @@ public class BankApp
         ba1.withdraw(20.00);                   // make withdrawal
 
         System.out.println("Im searching for transactions, please, wait");//lie lie lie
-        System.out.println("5 4 3 2 1");
-        System.out.print("Now your balance is : ");
+        Thread.sleep(1500);
+        System.out.print("5  ");
+        Thread.sleep(1500);
+        System.out.print("4  ");
+        Thread.sleep(1500);
+        System.out.print("3  ");
+        Thread.sleep(1500);
+        System.out.print("2  ");
+        Thread.sleep(1000);
+        System.out.println("1  ");
+        Thread.sleep(1000);
+        System.out.print("Your ");
         ba1.display();                         // display balance
     }  // end main()
 }
