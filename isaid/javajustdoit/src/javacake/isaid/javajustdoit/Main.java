79d5d8faef7af2479cac17b65a6933f9b33ae78c
Oxygen_Intoxication <yu-b@yandex.ru>
2018-04-28 19:51:12 +0300

    DEL fckn garbage

--- isaid/javajustdoit/src/javacake/isaid/javajustdoit/Main.java
+++
@@ -1,9 +0,0 @@
-package javacake.isaid.javajustdoit;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Java say hi");
-    }
-}
